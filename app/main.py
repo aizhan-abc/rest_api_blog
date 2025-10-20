c70b983f98961bc5b48a784415aba8c4064ff527
aizhanozbekova <aizhan.ozbekova.z@@gmail.com>
2025-10-20 22:22:19 +0600

    main

--- app/main.py
+++ app/main.py
@@ -18,20 +18,3 @@ def get_db():
 def read_posts(db: Session = Depends(get_db)):
     return crud.get_posts(db)
 
-@app.get("/posts/{post_id}", response_model=schemas.Post)
-def read_post(post_id: int, db: Session = Depends(get_db)):
-    db_post = crud.get_post(db, post_id)
-    if db_post is None:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return db_post
-
-@app.post("/posts", response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    return crud.create_post(db, post)
-
-@app.delete("/posts/{post_id}")
-def delete_post(post_id: int, db: Session = Depends(get_db)):
-    success = crud.delete_post(db, post_id)
-    if not success:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return {"message": "Post deleted successfully"}
